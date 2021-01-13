81d90bf5c20161161dddef16fc4f009954964d6a
Evelyn Yoa <yoaevelyn@gmail.com>
2021-01-13 23:13:46 +0000

    added wordsearch and test files

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -35,4 +35,37 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  it("should return true if the word is present and backwards", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'T', 'I', 'N', 'F', 'M', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'W', 'O', 'R', 'H', 'T', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'THROW')
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present and diagonal", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'E', 'Q', 'U', 'A', 'L'],
+      ['S', 'T', 'I', 'N', 'R', 'M', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'R', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'O', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'R'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'W', 'O', 'R', 'H', 'T', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'ERROR')
+
+    assert.isTrue(result);
+  });
+
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,81 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  //join the rows of the 2D array to make it easy to search for words
+  //the arrays are pre oriented in the suitable orientation.
+  
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  const verticalJoin = vertical(letters).map(ls => ls.join(''));
+  const hJoinReverse = reverseArray(horizontalJoin);
+  const vJoinReverse = reverseArray(verticalJoin);
+  const diagonalJoin = diagonal(letters);
+  // join all the lists so they can be searched at the same time.
+  const allLists = [horizontalJoin, verticalJoin, hJoinReverse, vJoinReverse, diagonalJoin];
+  const megaJoin = allLists.map(ls => ls.join(''));
+    
+  // look for the specified word
+  for (let el of megaJoin) {
+    if (el.includes(word)) return true;
+  }
 
-module.exports = wordSearch
+  return false;
+};
+
+//returns the diagonal values as an array
+const diagonal = function(arr) {
+  const x = arr[0].length;
+  const y = arr.length;
+  let result = [];
+  
+  // main diagonal values
+  let element1 = '';
+  for (let i = 0; i < y; i++) {
+    element1 += arr[i][i];
+  }
+  result.push(element1);
+  
+  //values for the other diagonals
+  for (let j = 1; j <= y; j++) {
+    let element2 = '';
+    let element3 = '';
+    for (let i = 0; i < x; i++) {
+      if (i + j < x) element2 += arr[i][i + j];
+      if (i + j < y) element3 += arr[i + j][i];
+    }
+    if (element2) result.push(element2);
+    if (element3) result.push(element3);
+  }
+
+  return result;
+};
+
+// returns the array in reverse without changing original
+const reverseArray = function(arr) {
+  let result = [];
+  for (const el of arr) {
+    let newOrder = '';
+    for (let i = el.length - 1; i >= 0; i--) {
+      newOrder += el[i];
+    }
+    result.push(newOrder);
+  }
+  return result;
+};
+
+
+// flips array to vertical orientation
+const vertical = function(matrix) {
+  let result = [];
+  for (let i = 0; i < matrix[0].length; i++) {
+    result.push([]);
+    for (let j = 0; j < matrix.length; j++) {
+      result[i].push(0);
+    }
+  }
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      result[j][i] = matrix[i][j];
+    }
+  }
+  return result;
+};
+
+module.exports = wordSearch;
